a891adaa84bfecaccb4be1f6b5c1fdc39ca2b638
Saksham5220 <63444444+Saksham5220@users.noreply.github.com>
2025-03-18 18:01:06 -0500

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -1,6 +1,6 @@
 # SEC API access credentials
 
 # User agent for SEC.gov access (required)
-user_agent = "Saksham Nirula (saakinrl@gmail.com)"
+user_agent = ""
 # API key for SEC-API.io
-api_key = "47b735fcfedd17e66521aea173891419d8e6b7b06d3093b65e8aabbf0d6dbfea"
+api_key = ""
